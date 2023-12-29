7e6220525e8305734a6ab64bce8a68ed09192133
shifumonitor <83143389+shifumonitor@users.noreply.github.com>
2023-12-28 20:17:11 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,11 +31,6 @@ export default function Component() {
               </Link>
             </li>
             <li>
-              <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-			  onClick={() => {}}
-			  type="button" >
-                Create QR code
-              </Button>
             </li>
           </ul>
         </nav>
