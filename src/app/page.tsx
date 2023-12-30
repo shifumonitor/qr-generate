2067fdc22d5c9f9d97c1a75f6dfad5bbc9d29576
shifumonitor <83143389+shifumonitor@users.noreply.github.com>
2023-12-29 18:44:01 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,13 @@
+'use client'
 
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/lgqGnZeOkk2
- */
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import Button from './components/ui/Button';
 import { Analytics } from '@vercel/analytics/react';
-
-export default function Component() {
-  return (
-  <div className="bg-white min-h-screen">
-    <div key="1" className="container bg-white mx-auto px-4 relative">
-      <header className="flex justify-between items-center py-4">
-        <h1 className="text-2xl text-gray-600 font-bold">QReator</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link className="text-base text-gray-600 hover:text-gray-800" href="#">
-                Pricing
-              </Link>
-            </li>
-            <li>
-              <Link className="text-base text-gray-600 hover:text-gray-800" href="#">
-                Log in
-              </Link>
-            </li>
-            <li>
-              <Link className="text-base text-gray-600 hover:text-gray-800" href="#">
-                Sign up
-              </Link>
-            </li>
-            <li>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main className="text-center mt-20 relative">
-        <h2 className="text-5xl text-gray-800 font-extrabold">
-          Creating QR codes made  
-          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-red-600 animate-hue animate-pulse">
-            {' easy & free '}
-          </span>
-        </h2>
-        <p className="mt-6 text-xl text-gray-700">
-          Say goodbye to complex QR codes. Meet QReator — the free, simple QR builder you have been looking for.
-        </p>
-        <Link href="/qr-generator">
-          <button className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded inline-flex items-center" type="button" >
-            <ArrowRightIcon className="ml-2" />
-          </button>
-        </Link>
-        <p className="mt-4 text-sm text-gray-500">No signup required</p>
-        <div className="mt-10">
-        </div>
-      </main>
-	  <Analytics />
-    </div>
-	</div>
-  )
-}
+import Modal from './components/ui/Modal'; // Import the modal component
+import YourFormComponent from './components/ui/YourFormComponent'; // Your form component
 
 interface ArrowRightIconProps {
-  // Add any specific props you expect here
-  // For example:
-  // className?: string;
-  [key: string]: any; // This allows any additional props
+  [key: string]: any;
 }
 
 function ArrowRightIcon(props: ArrowRightIconProps) {
@@ -85,5 +27,74 @@ function ArrowRightIcon(props: ArrowRightIconProps) {
       <path d="M5 12h14" />
       <path d="m12 5 7 7-7 7" />
     </svg>
+  );
+}
+
+export default function Component() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div className="bg-white min-h-screen">
+      <div key="1" className="container bg-white mx-auto px-4 relative">
+        <header className="flex justify-between items-center py-4">
+          <h1 className="text-2xl text-gray-600 font-bold">QR Code Builder</h1>
+          <nav>
+            <ul className="flex space-x-4">
+            <li>
+              <Link className="text-base text-gray-600 hover:text-gray-800" href="#">
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link className="text-base text-gray-600 hover:text-gray-800" href="#">
+                Log in
+              </Link>
+            </li>
+              <li>
+                <a
+                  className="text-base font-medium font-bold text-gray-600 hover:text-white hover:bg-gray-800 px-3 py-2 rounded-md transition duration-300 ease-in-out"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsModalOpen(true);
+                  }}
+                >
+                  Sign up
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="text-center mt-20 relative">
+        <h2 className="text-5xl text-gray-800 font-extrabold">
+          The
+          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-red-600 animate-hue animate-pulse">
+            {' quickest '}
+          </span>
+          way to build QR codes
+          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-red-600 animate-hue">
+            {' (for free!) '}
+          </span>
+        </h2>
+        <p className="mt-6 text-xl text-gray-700">
+          Build QR codes in seconds. Meet QR Code Builder — the free, simple QR generator you have been looking for.
+        </p>
+        <Link href="/qr-generator">
+          <button className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded inline-flex items-center" type="button" > Build QR Code
+            <ArrowRightIcon className="ml-2" />
+          </button>
+        </Link>
+        <p className="mt-4 text-sm font-semibold text-gray-500">No signup required</p>
+        <div className="mt-10">
+        </div>
+      </main>
+        <Analytics />
+      </div>
+
+      {/* Modal with Form */}
+      <Modal isOpen={isModalOpen} close={() => setIsModalOpen(false)}>
+        <YourFormComponent />
+      </Modal>
+    </div>
   );
 }
